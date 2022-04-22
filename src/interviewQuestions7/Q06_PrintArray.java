361f6c9b554278d12428c0954428aef9618c0197
Erol Evren <erlevren@gmail.com>
2022-04-22 16:41:11 +0300

    22/04

--- src/interviewQuestions7/Q06_PrintArray.java
+++ src/interviewQuestions7/Q06_PrintArray.java
@@ -7,12 +7,9 @@ public class Q06_PrintArray {
     public static void main(String[] args) {
         String [] arabalar = {"Honda", "Toyota", "Porsche", "Tesla", "BMW"};
         printElements(arabalar);
-
         String [] renkler = {"Mavi", "Kirmizi", "Mor", "Kahverengi", "Sari","Turuncu", "Yesil"};
         printElements(renkler);
-
     }
-
     private static void printElements(String[] arr) {
         for (int i=0;i<arr.length;i++) {
             System.out.print(arr[i] + " " ); //Honda Toyota Porsche Tesla BMW
